4c8311c45ba9835a57fc9418e75b6932b403aedc
Karol Garbocz <karolgarbocz@gmail.com>
2020-07-19 21:56:43 +0200

    .

---
+++ cmake-build-release/CMakeFiles/GameTemplate.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/karol/Desktop/XD/GamePlay/GamePlay.cpp" "C:/Users/karol/Desktop/XD/cmake-build-release/CMakeFiles/GameTemplate.dir/GamePlay/GamePlay.cpp.obj"
+  "C:/Users/karol/Desktop/XD/GamePlay/Player.cpp" "C:/Users/karol/Desktop/XD/cmake-build-release/CMakeFiles/GameTemplate.dir/GamePlay/Player.cpp.obj"
+  "C:/Users/karol/Desktop/XD/GamePlay/Przeszkoda.cpp" "C:/Users/karol/Desktop/XD/cmake-build-release/CMakeFiles/GameTemplate.dir/GamePlay/Przeszkoda.cpp.obj"
+  "C:/Users/karol/Desktop/XD/engine/GameManager.cpp" "C:/Users/karol/Desktop/XD/cmake-build-release/CMakeFiles/GameTemplate.dir/engine/GameManager.cpp.obj"
+  "C:/Users/karol/Desktop/XD/main.cpp" "C:/Users/karol/Desktop/XD/cmake-build-release/CMakeFiles/GameTemplate.dir/main.cpp.obj"
+  "C:/Users/karol/Desktop/XD/states/GamePlay/GamePlayController.cpp" "C:/Users/karol/Desktop/XD/cmake-build-release/CMakeFiles/GameTemplate.dir/states/GamePlay/GamePlayController.cpp.obj"
+  "C:/Users/karol/Desktop/XD/states/Menu/MenuController.cpp" "C:/Users/karol/Desktop/XD/cmake-build-release/CMakeFiles/GameTemplate.dir/states/Menu/MenuController.cpp.obj"
+  "C:/Users/karol/Desktop/XD/states/OptionsContoller/OptionsController.cpp" "C:/Users/karol/Desktop/XD/cmake-build-release/CMakeFiles/GameTemplate.dir/states/OptionsContoller/OptionsController.cpp.obj"
+  "C:/Users/karol/Desktop/XD/states/Scores/ScoresController.cpp" "C:/Users/karol/Desktop/XD/cmake-build-release/CMakeFiles/GameTemplate.dir/states/Scores/ScoresController.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "C:/SFMLGCC/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-release/CMakeFiles/GameTemplate.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GameTemplate.dir/GamePlay/GamePlay.cpp.obj"
+  "CMakeFiles/GameTemplate.dir/GamePlay/Player.cpp.obj"
+  "CMakeFiles/GameTemplate.dir/GamePlay/Przeszkoda.cpp.obj"
+  "CMakeFiles/GameTemplate.dir/engine/GameManager.cpp.obj"
+  "CMakeFiles/GameTemplate.dir/main.cpp.obj"
+  "CMakeFiles/GameTemplate.dir/states/GamePlay/GamePlayController.cpp.obj"
+  "CMakeFiles/GameTemplate.dir/states/Menu/MenuController.cpp.obj"
+  "CMakeFiles/GameTemplate.dir/states/OptionsContoller/OptionsController.cpp.obj"
+  "CMakeFiles/GameTemplate.dir/states/Scores/ScoresController.cpp.obj"
+  "GameTemplate.exe"
+  "GameTemplate.exe.manifest"
+  "GameTemplate.pdb"
+  "libGameTemplate.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GameTemplate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
